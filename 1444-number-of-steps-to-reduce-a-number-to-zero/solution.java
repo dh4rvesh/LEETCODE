c607f73558d7f3c89df20c1469c1101722873c16
dh4rvesh <126760381+dh4rvesh@users.noreply.github.com>
2025-12-16 11:34:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.1 MB (52.32%)

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        return steps(num);
+    }
+    public static int steps(int num){
+        return helper(num,0);
+    }
+    public static int helper(int num,int count){
+        if(num==0) return count;
+        if(num%2==0) return helper(num/2, ++count);
+        else return helper(num-1,++count);
+    }
+}
