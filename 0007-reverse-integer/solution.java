040d8c34a07a88e8114e8737191540b93d3ab199
dh4rvesh <126760381+dh4rvesh@users.noreply.github.com>
2025-12-16 10:32:20 +0000

    Sync LeetCode submission Runtime - 1 ms (95.23%), Memory - 42.8 MB (6.18%)

---
+++ 0007-reverse-integer/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        int temp=x;
+        long res=0;
+        while(temp!=0){
+            res=res*10 + temp%10;
+            temp/=10;
+        }
+        if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int) res;
+    }
+}
