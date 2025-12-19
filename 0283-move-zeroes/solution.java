83f3308e676d5390270fa6812f6c824e9d08c6ab
dh4rvesh <126760381+dh4rvesh@users.noreply.github.com>
2025-12-19 05:34:41 +0000

    Sync LeetCode submission Runtime - 1 ms (99.76%), Memory - 47.9 MB (22.67%)

---
+++ 0283-move-zeroes/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=0;
+        for(int i:nums){
+            if(i!=0){
+                nums[j++]=i;
+            }
+        }
+        while(j<nums.length){
+            nums[j++]=0;
+        } 
+    }
+}
