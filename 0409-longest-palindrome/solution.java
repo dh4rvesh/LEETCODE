1a68cb09a8eaa8bd505dcbed1c95f7aed83a2f06
dh4rvesh <126760381+dh4rvesh@users.noreply.github.com>
2025-12-24 03:07:59 +0000

    Sync LeetCode submission Runtime - 1 ms (99.88%), Memory - 42.9 MB (80.91%)

---
+++ 0409-longest-palindrome/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] freq = new int[128];
+        for(char c:s.toCharArray()){
+            freq[c]++;
+        }
+        int length=0;
+        for(int count:freq){
+            if(count%2==0){
+                length+=count;
+            }
+            else{
+                length+=count-1;
+            }
+        }
+        if(length<s.length()){
+            length+=1;
+        }
+        return length;
+    }
+}
